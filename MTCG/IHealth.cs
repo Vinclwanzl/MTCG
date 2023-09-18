5c4bd98a146526c3807a2f68d5c5dd19bfe91767
Vincwanz <if23b188@technikum-wien.at>
2023-09-18 19:15:43 +0200

    Add project files.

---
+++ MTCG/IHealth.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTCG
+{
+    
+    interface IHealth
+    {   
+        private void recieveDamage(int amount) { }
+        private void recieceHealing(int amount) { }
+    }
+}
